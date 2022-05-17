0a7115e8e464790eb0cfdfaa4f760c980c89304a
Stefan Lachmann <stefanlachmann@hotmail.com>
2022-05-17 15:59:42 +0200

    setup terraform build infrastructure change infra destroy infra define input variables Query data with output vars
    ...
    store remote state in terraform could

---
+++ README.md
@@ -0,0 +1,4 @@
+solutions from:
+Get Started - Azure
+https://learn.hashicorp.com/collections/terraform/azure-get-started
+
---
+++ main.tf
@@ -0,0 +1,42 @@
+# Configure the Azure provider
+# https://learn.hashicorp.com/tutorials/terraform/azure-build}
+# terraform blocket är specifika terraforms settings
+
+terraform {
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "karma-limitied"
+    workspaces {
+      name = "learn-terraform-azure"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "westeurope"
+
+  tags = {
+    Enviroment = "Terraform getting started"
+    Team       = "DevOps"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westeurope"
+  resource_group_name = azurerm_resource_group.rg.name
+}
